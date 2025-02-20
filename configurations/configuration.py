37cd2de7df4023e43ebc51e14bf136d124468af2
bel_p <bel_pavel@mail.ru>
2025-02-20 09:17:55 +0300

    After edits

--- configurations/configuration.py
+++ configurations/configuration.py
@@ -6,8 +6,10 @@ from utilities.deserialize_json import DeserializeJson
 
 class ConfigManager:
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    config_file_path = Path(os.path.join(BASE_DIR, "../resourses", "test_data.json")).resolve()
+    config_file_path = Path(os.path.join(BASE_DIR, "../resourses", "config.json")).resolve()
 
     @classmethod
     def get_config_data(cls):
         return ConfigModel.model_validate_json(DeserializeJson().deserialize_json(cls.config_file_path))
+
+print(ConfigManager().config_file_path)
